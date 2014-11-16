a208e5d0bc33954c61f340eb555d5eedbf4b52f7
Tony198x <fly_ontheway@163.com>
2014-11-16 11:14:20 +0800

    Add a project
    All project files include

---
+++ MvcApp.Dotaba/App_Start/BundleConfig.cs
@@ -0,0 +1,57 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace MvcApp.Dotaba
+{
+    public class BundleConfig
+    {
+        // 有关 Bundling 的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            #region JS源的绑定:居然不支持加载压缩版的资源（js/css），路径必须指定正确，在页面加载的时候绑定静态资源用。
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                        "~/Scripts/jquery-ui-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.unobtrusive*",
+                        "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                        "~/Bootstrap/js/bootstrap.js"
+                ));
+            // 使用 Modernizr 的开发版本进行开发和了解信息。然后，当你做好
+            // 生产准备时，请使用 http://modernizr.com 上的生成工具来仅选择所需的测试。
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/Scripts/modernizr-*"));
+
+            #endregion
+
+            #region CSS源的绑定
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+                        "~/Content/themes/base/jquery.ui.core.css",
+                        "~/Content/themes/base/jquery.ui.resizable.css",
+                        "~/Content/themes/base/jquery.ui.selectable.css",
+                        "~/Content/themes/base/jquery.ui.accordion.css",
+                        "~/Content/themes/base/jquery.ui.autocomplete.css",
+                        "~/Content/themes/base/jquery.ui.button.css",
+                        "~/Content/themes/base/jquery.ui.dialog.css",
+                        "~/Content/themes/base/jquery.ui.slider.css",
+                        "~/Content/themes/base/jquery.ui.tabs.css",
+                        "~/Content/themes/base/jquery.ui.datepicker.css",
+                        "~/Content/themes/base/jquery.ui.progressbar.css",
+                        "~/Content/themes/base/jquery.ui.theme.css"));
+
+            bundles.Add(new StyleBundle("~/Bootstrap/css").Include(
+                        "~/Bootstrap/css/bootstrap-theme.css",
+                        "~/Bootstrap/css/bootstrap.css"
+                ));
+            #endregion
+            
+        }
+    }
+}
---
+++ MvcApp.Dotaba/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcApp.Dotaba.MvcApplication" Language="C#" %>
